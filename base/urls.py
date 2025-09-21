952c09e05abc58371b03079823648d062391a1e3
shinekahehe <sri.shineka@gmail.com>
2025-09-21 19:42:37 +0530

    updated urls

--- base/urls.py
+++ base/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('', views.home, name='home'),
     path('admin-student-portal/', views.home, name='admin-student-portal'),
     path('login/', views.login_view, name='login'),
     path('signup/', views.signup_view, name='signup'),
